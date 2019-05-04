1e9b19052f334eeec69f6965f072948a8933abc1
Eric Schles <ericschles@gmail.com>
2019-05-03 20:21:21 -0400

    type: refactor description: refactored tests and added one files added: None

--- tests/test_normalization.py
+++ tests/test_normalization.py
@@ -1,19 +1,30 @@
 from text_processing.normalization import NormalizeText
 
+def test_strip_punctuation():
+    norm = NormalizeText()
+    word, punctuation = norm.strip_punctuation("Hello,",
+                                               return_punctuation=True)
+    assert word == "Hello"
+    assert punctuation == ","
+
 def test_correct_whitespace():
-    norm = NormalizeText(check_whitespace=True)
-    norm.fit("Hello there friends")
-    assert norm.transform() == "Hello there friends"
+    norm = NormalizeText()
+    assert norm.correct_whitespace("Hello there  friends") == "Hello there friends"
 
 def test_case_normalization():
-    norm = NormalizeText(norm_case=True)
-    norm.fit("Hello there friends")
-    assert norm.transform() == "hello there friends"
+    norm = NormalizeText()
+    assert norm.normalize_case("HELLO THeRe FRiENDS") == "hello there friends"
 
 def test_initialize_spellchecker_simple():
-    norm = NormalizeText(check_spelling=True)
+    norm = NormalizeText()
     try:
         norm.initialize_spellchecker()
         assert True
     except:
         assert False
+
+def test_add_stopwords():
+    norm = NormalizeText()
+    norm.add_to_stopwords(words=["goat", "bob", "phil"])
+    assert norm.remove_stopwords("Hey bob, get the goat from phil") == "Hey"
+
