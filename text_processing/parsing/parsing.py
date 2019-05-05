94c4658b2b555f9937d985797233d7288b120b6f
Eric Schles <ericschles@gmail.com>
2019-05-05 11:34:43 -0400

    type: feature description: adding tfidf and associated methods files added: None

--- text_processing/parsing/parsing.py
+++ text_processing/parsing/parsing.py
@@ -1,11 +1,38 @@
-class ParseText:
-    def __init__(self, corpus=None):
-        self.corpus = corpus
+import nltk
+import string
+import os
 
+from .normalization import NormalizeText
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.stem.porter import PorterStemmer
+
+class ParseText:
+    def __init__(self):
+        self.stemmer = PorterStemmer()
+        self.normalizer = NormalizeText()
+        
+    def stem_tokens(self, tokens):
+        stemmed = []
+        for item in tokens:
+            stemmed.append(self.stemmer.stem(item))
+        return stemmed
+
+    def tokenize(self, text):
+        tokens = nltk.word_tokenize(text)
+        stems = self.stem_tokens(tokens)
+        return stems
+
+    def process_text(self, text):
+        text = text.lower()
+        text = " ".join(
+            [self.normalizer.strip_punctuation(word)
+             for word in text.split(" ")]
+        )
+        
     def tfidf(self, text):
-        # implement this:
-        # https://www2.cs.duke.edu/courses/spring14/compsci290/assignments/lab02.html
-        pass
+        text = self.process_text(text)
+        tfidf_vectorizer = TfidfVectorizer(tokenizer=self.tokenize, stop_words='english')
+        return tfs = tfidf_vectorizer.fit_transform(text)
 
     # to do
     # add spell checker
